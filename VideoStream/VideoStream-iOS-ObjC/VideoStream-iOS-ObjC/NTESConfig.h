2de8079dce920261385281dd85620befdf54c039
Think <zhangyulong03@corp.netease.com>
2020-09-02 15:05:52 +0800

    Think: add video stream proj;

---
+++ VideoStream/VideoStream-iOS-ObjC/VideoStream-iOS-ObjC/NTESConfig.h
@@ -0,0 +1,15 @@
+//
+//  NTESConfig.h
+//  VideoStream-iOS-ObjC
+//
+//  Created by Think on 2020/9/2.
+//  Copyright © 2020 丁文超. All rights reserved.
+//
+
+#ifndef NTESConfig_h
+#define NTESConfig_h
+
+#define kAppKey @"<#请替换你的AppKey#>"
+#define kStreamURL @"<#请替换您的推流地址#>"
+
+#endif /* NTESConfig_h */
