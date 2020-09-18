632fdcbe8e70b6a0e80d86e66ba96f167e2a527d
AceHall <yulongmailbox@163.com>
2020-09-18 18:57:10 +0800

    Update config_stub.py

--- Beauty/Beauty-iOS-ObjC/Workflow/config_stub.py
+++ Beauty/Beauty-iOS-ObjC/Workflow/config_stub.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 # Read heaer file
-APP_KEY_FILE = os.environ['APP_KEY_HEADER_PATH']
+APP_KEY_FILE = os.environ['APP_CONFIG_HEADER_PATH']
 if APP_KEY_FILE is None:
 	print "APP_KEY_FILE Not found"
 	sys.exit(1)
@@ -22,7 +22,7 @@ if APP_KEY is None:
 	sys.exit(1)
 
 # Stub valid key and write back
-ret = re.sub('AppKey\s@\"\S*\"', 'AppKey @\"' + APP_KEY + '\"', text)
+ret = re.sub('kAppKey\s@\"\S*\"', 'kAppKey @\"' + APP_KEY + '\"', text)
 io.seek(0)
 io.write(ret)
 io.truncate()
