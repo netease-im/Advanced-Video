fcbdc731584dd3f1d42e06df2628add3b163447f
houchengdong <houchengdong@corp.netease.com>
2020-10-15 10:22:10 +0800

    update config.cpp

--- AudioMixing/AudioMixing-Android-Java/app/src/main/cpp/config.cpp
+++ AudioMixing/AudioMixing-Android-Java/app/src/main/cpp/config.cpp
@@ -9,6 +9,6 @@ extern "C"
 JNIEXPORT jstring
 JNICALL
 Java_com_netease_nmc_nertcsample_audiomixing_NativeConfig_getAppKey__(JNIEnv *env, jclass) {
-    std::string appKey = "set you APP key here";
+    std::string appKey = "请输入您的AppKey";
     return env->NewStringUTF(appKey.c_str());
 }
