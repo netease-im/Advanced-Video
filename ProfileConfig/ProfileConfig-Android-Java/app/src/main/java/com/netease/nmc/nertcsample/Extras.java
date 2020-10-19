b39f431af13777e77408959f829e91d685d4ac67
hzsusu <hzsusu@corp.netease.com>
2020-10-19 14:34:29 +0800

    android: add back profileconfig (avmanage)

---
+++ ProfileConfig/ProfileConfig-Android-Java/app/src/main/java/com/netease/nmc/nertcsample/Extras.java
@@ -0,0 +1,6 @@
+package com.netease.nmc.nertcsample;
+
+public interface Extras {
+    String USER_ID = "user_id";
+    String ROOM_ID = "room_id";
+}
---
+++ ProfileConfig/ProfileConfig-Android-Java/app/src/main/java/com/netease/nmc/nertcsample/settings/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.netease.nmc.nertcsample.settings;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.netease.nmc.nertcsample.R;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_settings);
+    }
+}
