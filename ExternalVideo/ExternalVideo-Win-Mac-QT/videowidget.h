4b6efaadd5ffd7580abb6c67479585ef28ec189b
liangpeng <liangpeng01@corp.netease.com>
2020-11-24 18:41:33 +0800

    提交自定义视频采集工程

---
+++ ExternalVideo/ExternalVideo-Win-Mac-QT/videowidget.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <QWidget>
+#include "ui_videowidget.h"
+
+class VideoWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    VideoWidget(QWidget *parent = Q_NULLPTR);
+    ~VideoWidget();
+public:
+    void setUsrID(QString strid);
+    QString getUserID();
+    void closeRender();
+    void* getVideoHwnd();
+protected:
+private:
+    Ui::videowidget ui;
+    QString m_uid;
+};
