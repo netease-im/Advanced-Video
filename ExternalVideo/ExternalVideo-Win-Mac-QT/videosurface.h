3c9475ad2083432f53493e1665f8fce638fe9032
liangpeng <liangpeng01@corp.netease.com>
2020-11-24 17:34:13 +0800

    提交自定义视频采集

---
+++ ExternalVideo/ExternalVideo-Win-Mac-QT/videosurface.h
@@ -0,0 +1,20 @@
+﻿#ifndef VIDEOSURFACE_H
+#define VIDEOSURFACE_H
+
+#include <QAbstractVideoSurface>
+#include <QObject>
+
+class VideoSurface : public QAbstractVideoSurface {
+    Q_OBJECT
+public:
+    explicit VideoSurface(QObject* parent = nullptr);
+
+    QList<QVideoFrame::PixelFormat> supportedPixelFormats(QAbstractVideoBuffer::HandleType handleType = QAbstractVideoBuffer::NoHandle) const;
+    bool present(const QVideoFrame& frame);
+
+    bool isFormatSupported(const QVideoSurfaceFormat &format){return true;};
+signals:
+    void frameAvailable(QVideoFrame& frame);
+};
+
+#endif  // VIDEOSURFACE_H
