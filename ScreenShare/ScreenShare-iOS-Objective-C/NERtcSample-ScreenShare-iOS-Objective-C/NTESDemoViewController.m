47a1bd9f9cbd569008a7ff0f99310469d80ba4d3
yuan <allday2011@163.com>
2021-04-22 16:47:46 +0800

    iOS 屏幕共享sample 更新 4.0.3 SDK，同时支持视频采集和屏幕共享

--- ScreenShare/ScreenShare-iOS-Objective-C/NERtcSample-ScreenShare-iOS-Objective-C/NTESDemoViewController.m
+++ ScreenShare/ScreenShare-iOS-Objective-C/NERtcSample-ScreenShare-iOS-Objective-C/NTESDemoViewController.m
@@ -19,6 +19,9 @@ static void *KVOContext = &KVOContext;
 @property (nonatomic, strong) NSUserDefaults *userDefaults;
 @property (nonatomic, strong) NSNumber *currentUserID; //!< 当前用户ID
 
+@property (nonatomic, assign) BOOL cameraOn;
+@property (nonatomic, assign) BOOL screenShareOn;
+
 @property (strong, nonatomic) IBOutlet UITextField *userIDTextField;
 @property (strong, nonatomic) IBOutlet UITextField *roomIDTextField;
 @property (strong, nonatomic) IBOutlet UIButton *joinButton;
@@ -26,6 +29,10 @@ static void *KVOContext = &KVOContext;
 @property (strong, nonatomic) IBOutlet UIView *localUserView;
 @property (strong, nonatomic) IBOutlet UIView *remoteUserView;
 
+@property (weak, nonatomic) IBOutlet UIView *localScreenShareView;
+@property (weak, nonatomic) IBOutlet UIView *remoteScreenShareView;
+@property (weak, nonatomic) IBOutlet UIButton *cameraButton;
+@property (weak, nonatomic) IBOutlet UIButton *screenShareButton;
 
 - (IBAction)onJoinClick:(id)sender;
 - (IBAction)onLeaveClick:(id)sender;
@@ -82,14 +89,26 @@ static void *KVOContext = &KVOContext;
 
 - (void)onNERtcEngineUserDidJoinWithUserID:(uint64_t)userID userName:(NSString *)userName
 {
+    //设置对方摄像头画布
     NERtcVideoCanvas *canvas = [[NERtcVideoCanvas alloc] init];
     canvas.container = self.remoteUserView;
     [NERtcEngine.sharedEngine setupRemoteVideoCanvas:canvas forUserID:userID];
+    
+    //设置对方屏幕共享画布
+    NERtcVideoCanvas *subCanvas = [[NERtcVideoCanvas alloc] init];
+    subCanvas.container = self.remoteScreenShareView;
+    [NERtcEngine.sharedEngine setupRemoteSubStreamVideoCanvas:subCanvas forUserID:userID];
 }
 
 - (void)onNERtcEngineUserVideoDidStartWithUserID:(uint64_t)userID videoProfile:(NERtcVideoProfileType)profile
 {
     [NERtcEngine.sharedEngine subscribeRemoteVideo:YES forUserID:userID streamType:kNERtcRemoteVideoStreamTypeHigh];
+}
+
+- (void)onNERtcEngineUserSubStreamDidStartWithUserID:(uint64_t)userID subStreamProfile:(NERtcVideoProfileType)profile
+{
+    //订阅对方屏幕共享流
+    [NERtcEngine.sharedEngine subscribeRemoteSubStreamVideo:YES forUserID:userID];
 }
 
 - (void)onNERtcEngineDidLeaveChannelWithResult:(NERtcError)result
@@ -99,11 +118,30 @@ static void *KVOContext = &KVOContext;
     }
 }
 
+- (IBAction)onCameraButtonClick:(id)sender {
+    [NERtcEngine.sharedEngine enableLocalVideo:!_cameraOn];
+    _cameraOn = !_cameraOn;
+}
+
+- (IBAction)onScreenShareButtonClick:(id)sender {
+    //开关屏幕共享流，屏幕共享数据源需要自己手动开关，不由代码控制
+    if (_screenShareOn) {
+        [NERtcEngine.sharedEngine stopScreenCapture];
+    }
+    else{
+        NERtcVideoSubStreamEncodeConfiguration *config = [[NERtcVideoSubStreamEncodeConfiguration alloc] init];
+        [NERtcEngine.sharedEngine startScreenCapture:config];
+    }
+    _screenShareOn = !_screenShareOn;
+}
+
 - (IBAction)onJoinClick:(id)sender
 {
     if (self.userIDTextField.text.length == 0 || self.roomIDTextField.text.length == 0) {
         return;
     }
+    [self willJoinChannel];
+    
     [self.view endEditing:YES];
     uint64_t userID = self.userIDTextField.text.longLongValue;
     NSString *roomID = self.roomIDTextField.text;
@@ -116,9 +154,15 @@ static void *KVOContext = &KVOContext;
             return;
         }
         sself.currentUserID = @(userID);
+        //主流摄像头画布
         NERtcVideoCanvas *canvas = [[NERtcVideoCanvas alloc] init];
         canvas.container = sself.localUserView;
         [NERtcEngine.sharedEngine setupLocalVideoCanvas:canvas];
+        
+        //屏幕共享画布
+        NERtcVideoCanvas *subCanvas = [[NERtcVideoCanvas alloc] init];
+        subCanvas.container = sself.localScreenShareView;
+        [NERtcEngine.sharedEngine setupLocalSubStreamVideoCanvas:subCanvas];
         
         [sself showAlertMessage:@"Success!"];
         
@@ -150,9 +194,18 @@ static void *KVOContext = &KVOContext;
     context.engineDelegate = self;
     context.appKey = kAppKey;
     [coreEngine setupEngineWithContext:context];
-    [coreEngine setExternalVideoSource:YES];
+}
+
+- (void)willJoinChannel
+{
+    NERtcEngine *coreEngine = [NERtcEngine sharedEngine];
+    [coreEngine setExternalVideoSource:YES isScreen:YES];
+    NERtcVideoSubStreamEncodeConfiguration *config = [[NERtcVideoSubStreamEncodeConfiguration alloc] init];
+    [coreEngine startScreenCapture:config];
     [coreEngine enableLocalAudio:YES];
     [coreEngine enableLocalVideo:YES];
+    _cameraOn = YES;
+    _screenShareOn = YES;
 }
 
 - (void)addSystemBroadcastPickerIfPossible
