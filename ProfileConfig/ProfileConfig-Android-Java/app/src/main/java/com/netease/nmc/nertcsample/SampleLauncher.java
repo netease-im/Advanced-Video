c387623f4e07dc9f50c7f99f45bdbb31688ca730
zhouyibin <zhouyibin@corp.netease.com>
2021-08-27 17:11:15 +0800

    update ProfileConfig-Android-Java's nertc version to 4.3.8

--- ProfileConfig/ProfileConfig-Android-Java/app/src/main/java/com/netease/nmc/nertcsample/SampleLauncher.java
+++ ProfileConfig/ProfileConfig-Android-Java/app/src/main/java/com/netease/nmc/nertcsample/SampleLauncher.java
@@ -4,13 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 
 public class SampleLauncher {
-    public static void startBasic(Context context, String roomId, long userId) {
-        Intent intent = new Intent(context, BasicActivity.class);
-        intent.putExtra(Extras.ROOM_ID, roomId);
-        intent.putExtra(Extras.USER_ID, userId);
-        context.startActivity(intent);
-    }
-
     public static void startProfileConfig(Context context, String roomId, long userId) {
         Intent intent = new Intent(context, ProfileConfigActivity.class);
         intent.putExtra(Extras.ROOM_ID, roomId);
